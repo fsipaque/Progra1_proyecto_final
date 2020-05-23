7e557cd9ce612644567561a7a295a8ffab094213
Fernando <josefperez84@gmail.com>
2020-05-23 11:51:35 -0600

    Creación de proyecto

---
+++ src/main/java/proyecto1/jfperez/progra1_pro_final/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1.jfperez.progra1_pro_final;
+
+/**
+ *
+ * @author josep
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
