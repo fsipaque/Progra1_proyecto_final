78c73d6f759bf7dcd76060e4207ad452072b17de
Fernando <josefperez84@gmail.com>
2020-06-06 22:42:29 -0600

    Panel de entidades

---
+++ src/main/java/proyecto1/jfperez/progra1_pro_final/Entidad.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1.jfperez.progra1_pro_final;
+
+/**
+ *
+ * @author josep
+ */
+public class Entidad {
+
+    private int indice;
+    private String nombre;
+    private int cantidad;
+    private int posicion;
+
+    /**
+     * @return the indice
+     */
+    public int getIndice() {
+        return indice;
+    }
+
+    /**
+     * @param indice the indice to set
+     */
+    public void setIndice(int indice) {
+        this.indice = indice;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * @return the posicion
+     */
+    public int getPosicion() {
+        return posicion;
+    }
+
+    /**
+     * @param posicion the posicion to set
+     */
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+    
+    
+    
+}
